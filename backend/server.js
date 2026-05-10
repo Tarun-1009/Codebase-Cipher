0a47c240b817c0047dc8eab58ee5a96c06a6d6b6
Tarun-1009 <tarunkumar.pkl1009@gmail.com>
2026-05-10 23:16:54 +0530

    repository JSON data fetch

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { getRepo } = require("./src/services/githubService");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Codebase Cipher Backend is Running!");
+});
+
+
+//temporary - data directly send to frontend just to check
+
+app.get("/analyze/:username/:repo",async (req,res)=>{
+
+    const {username,repo}=req.params;
+    try {
+        const tree=await getRepo(username,repo);
+        res.json(tree);
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
